a8724228937dc61c5a92659a714bb803e439bbb6
Tristan Ross <tristan.ross@midstall.com>
2022-06-10 23:05:42 -0700

    feat: added first timing

--- src/http/middleware/auth.ts
+++ src/http/middleware/auth.ts
@@ -1,5 +1,6 @@
 import deepmerge from 'deepmerge'
 import { Request, Response, NextFunction } from 'express'
+import { resolve } from 'path'
 import { checkAccessToken } from '../../lib/accesstoken'
 import { HttpUnauthorizedError } from '../exceptions'
 
@@ -11,15 +12,19 @@ export function authHandler(opts?: AuthHandlerOptions) {
   opts = deepmerge(opts || {}, {
     required: true,
   })
-  return (req: Request, _res: Response | null, next: NextFunction) => {
+  return (req: Request, res: Response | null, next: NextFunction) => {
     const header = req.headers.authorization
 
     if (typeof header === 'undefined' && opts?.required) {
       next(new HttpUnauthorizedError('Not authorization header was given'))
     } else if (typeof header === 'string') {
       const run = async () => {
+        if (res) res.startTime('auth', 'Authentication Timing')
+
         const accessToken = await checkAccessToken(header)
         req.auth = { accessToken, user: accessToken.user }
+
+        if (res) res.endTime('auth')
         next()
       }
       run().catch((e) => next(e))
